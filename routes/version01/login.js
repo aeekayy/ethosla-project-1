2d05a840b048789a21a333df456938b5715d2a2a
MEAN User <mean@ip-172-26-8-103.ec2.internal>
2017-06-04 16:23:15 +0000

    Adding login capability.

--- routes/version01/login.js
+++ routes/version01/login.js
@@ -1,23 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
-var CognitoStrategy = require('passport-cognito');
 
 /**
  * Initiate the Cognito Strategy for 
  * Login of Dhanjo
  */
-passport.use(new CognitoStrategy({
-	userPoolId: 'us-west-2_VOxqkVzlh',
-	clientId: '35iv5ri622b4o6agrul7mrpkhs',
-	region: 'us-west-2'
-  }, 
-  function(accessToken, idToken, refreshToken, user, cb) {
-    process.nextTick(function() {
-      return cb(null, user); 
-    });
-  }
-));
+// var AmazonCognitoIdentity = require('aws-sdk/clients/cognitoidentity');
+// var CognitoIdentityServiceProvider = require('aws-sdk/clients/cognitoidentityserviceprovider');
+var AWSCognito = require('amazon-cognito-identity-js');
+var CognitoUserPool = AWSCognito.CognitoUserPool;
+
+var poolData = { 
+  UserPoolId: 'us-west-2_VOxqkVzlh',
+  ClientId:  '35iv5ri622b4o6agrul7mrpkhs'
+};
+// var userPool = new CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+var userPool = new AWSCognito.CognitoUserPool(poolData);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,10 +23,30 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  res.send(passport.authenticate('cognito', {
-    successRedirect: '/',
-    failureRedirect: '/api/v1/login'
-  }));
+  /** Todo tasks
+   * 
+   * Check for empty username and password
+   */
+  var authenticationData = {
+    Username: req.body.username, 
+    Password: req.body.password
+  }
+
+  var authenticationDetails = new AWSCognito.AuthenticationDetails(authenticationData);
+  var userData = {
+    Username: req.body.username, 
+    Pool: userPool
+  }; 
+
+  var cognitoUser = new AWSCognito.CognitoUser(userData);
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function(result) { 
+      res.send('{ "Response": "success", "AccessToken": "' + result.getAccessToken().getJwtToken() + '" }');
+    },
+    onFailure: function(err) { 
+	res.send('{ "Response": "error", "Message": "' + err + '" }');
+    }
+  });
 });
 
 module.exports = router;
